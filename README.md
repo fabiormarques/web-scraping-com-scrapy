5416270d47b0ed08a817ac0e06ddc956abbbf8cc
Fabio Rodrigo Marques <fabiormarques@gmail.com>
2024-05-30 16:06:56 -0400

    adicionar transformação com pandas

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Para executar a extração dos dados, executar o comando abaixo dentro da pasta 
 ```bash
 scrapy crawl mercadolivre -o ../data/data.jsonl
 ```
+
+Para rodar o PANDAS, executar o comando abaixo na pasta principal do projeto
+
+```bash
+python src/transformacao/main.py
+```
---
+++ src/transformacao/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import sqlite3
+from datetime import datetime
+
+df = pd.read_json("data/data.jsonl", lines=True)
+
+pd.options.display.max_columns = None
+
+df["_source"] = "https://lista.mercadolivre.com.br/tenis-corrida-masculino"
+df["_data_coleta"] = datetime.now()
+
+df["old_price_reais"] = df["old_price_reais"].fillna(0).astype(float)
+df["old_price_centavos"] = df["old_price_centavos"].fillna(0).astype(float)
+df["new_price_reais"] = df["new_price_reais"].fillna(0).astype(float)
+df["new_price_centavos"] = df["new_price_centavos"].fillna(0).astype(float)
+df["reviews_rating_number"] = df["reviews_rating_number"].fillna(0).astype(float)
+
+df["reviews_amount"] = df["reviews_amount"].str.replace("[\(\)]", '', regex=True)
+df["reviews_amount"] = df["reviews_amount"].fillna(0).astype(int)
+
+df["old_price"] = df["old_price_reais"] + df["old_price_centavos"] / 100
+df["new_price"] = df["new_price_reais"] + df["new_price_centavos"] / 100
+
+df.drop(columns=["old_price_reais", "old_price_centavos", "new_price_reais", "new_price_centavos"], inplace=True)
+
+conn = sqlite3.connect("data/quotes.db")
+
+df.to_sql("mercadolivre_itens", conn, if_exists="replace", index=False)
+conn.close()
+
+
+print(df)
+
